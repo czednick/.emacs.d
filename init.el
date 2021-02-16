c8ab931010922ce05d96e5744a0cbbb5b5228d10
Casey Zednick <casey@zednick.com>
2021-02-16 15:29:23 -0800

    fix: update theme and use ido.

--- init.el
+++ init.el
@@ -1,53 +1,44 @@
 ;;
-;; Casey Zednick's init.el  
+;; Casey Zednick's init.el
 ;;
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-; fetch the list of packages available 
-(unless package-archive-contents
-  (package-refresh-contents))
 
-(defvar my-packages '(starter-kit
-                      auto-complete
-                      color-theme
-                      twilight-theme
-                      markdown-mode
-		      haskell-mode
-                      p4)
+(defvar my-packages '(color-theme-sanityinc-tomorrow
+                      magit)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
-     (package-install p)))
+    (package-install p)))
+
+
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
 
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
-
 (put 'downcase-region 'disabled nil)
-(setq-default ispell-program-name "/usr/bin/env aspell")
-
-(setq c-basic-indent 4)
+(setq-default ispell-program-name "/usr/local/bin/aspell")
+(setq c-basic-indent 2)
 (setq tab-width 4)
 (setq-default indent-tabs-mode nil) ;; No tabs anywhere!
-
 (set-face-attribute 'default nil :font "-apple-Menlo-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1" :height 140)
-;; No visual bell
+
 (setq ring-bell-function 'ignore)
-(menu-bar-mode 1)
 
 (setq color-theme-is-global t)
-(load-theme 'twilight t)
 
 (require 'recentf)
 (recentf-mode 1)
@@ -56,3 +47,58 @@
 
 (setq compilation-window-height 16) ;; Normaly takes half the window.
 (setq gdb-window-height 16) ;; Normaly takes half the window.
+(global-set-key [(meta g)] `goto-line)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#373b41" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#c5c8c6"))
+ '(ansi-term-color-vector
+   [unspecified "#14191f" "#d15120" "#81af34" "#deae3e" "#7e9fc9" "#a878b5" "#7e9fc9" "#dcdddd"])
+ '(beacon-color "#cc6666")
+ '(custom-enabled-themes '(sanityinc-tomorrow-night))
+ '(custom-safe-themes
+   '("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "51e228ffd6c4fff9b5168b31d5927c27734e82ec61f414970fc6bcce23bc140d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
+ '(fci-rule-character-color "#192028")
+ '(fci-rule-color "#373b41")
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(frame-background-mode 'dark)
+ '(package-selected-packages
+   '(haskell-mode color-theme-sanityinc-tomorrow twilight-anti-bright-theme magit twilight-theme))
+ '(safe-local-variable-values
+   '((haskell-process-use-ghci . t)
+     (haskell-indent-spaces . 4)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#cc6666")
+     (40 . "#de935f")
+     (60 . "#f0c674")
+     (80 . "#b5bd68")
+     (100 . "#8abeb7")
+     (120 . "#81a2be")
+     (140 . "#b294bb")
+     (160 . "#cc6666")
+     (180 . "#de935f")
+     (200 . "#f0c674")
+     (220 . "#b5bd68")
+     (240 . "#8abeb7")
+     (260 . "#81a2be")
+     (280 . "#b294bb")
+     (300 . "#cc6666")
+     (320 . "#de935f")
+     (340 . "#f0c674")
+     (360 . "#b5bd68")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(add-hook 'write-file-hooks 'delete-trailing-whitespace)
